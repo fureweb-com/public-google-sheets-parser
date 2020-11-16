81ac79594c88f0f3e349eaf172f24ac2e198105e
fureweb-com <fureweb.com@gmail.com>
2020-11-16 17:19:20 +0900

    Release v1.0.0
    - Currently, it is only available in the browser via distribution file.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Public Google sheets parser for browser
+---
+
+It is a simple parser for browser that helps you use public Google sheets document as if they were a database.
+
+The document to be used must be a Google Sheets document in the 'public' state and have a header in the first row. (e.g. [Google sheets for example](https://docs.google.com/spreadsheets/d/10WDbAPAY7Xl5DT36VuMheTPTTpqx9x0C5sDCnh4BGps/edit#gid=1839148703))
+
+It does not work in browsers where the [fetch API](https://caniuse.com/fetch) is not available.
+
+**Currently, it is only available in the browser through the distribution file.**
+
+### Usage example
+---
+
+```js
+// import distribution file before make GooglePublicSheetsParser instance. (see /dist/index.js)
+
+const sheetsId = '10WDbAPAY7Xl5DT36VuMheTPTTpqx9x0C5sDCnh4BGps'
+const parser = new GooglePublicSheetsParser(sheetsId)
+parser.parse().then((items) => {
+  console.log(items) // [{ a: 1, b: 2, c: 3}, { a: 4, b: 5, c: 6 }, ...]
+})
+```
+
+**That's it!**
---
+++ src/index.js
@@ -0,0 +1,57 @@
+class GooglePublicSheetsParser {
+  constructor(sheetsId) {
+    if (!sheetsId) throw new Error('SheetId is required.')
+    this.id = sheetsId
+  }
+
+  getSheetsData() {
+    // Read data from the first sheet of the target document.
+    // It cannot be used unless everyone has been given read permission.
+    // It must be a spreadsheet document with a header, as in the example document below.
+    // spreadsheet document for example: https://docs.google.com/spreadsheets/d/10WDbAPAY7Xl5DT36VuMheTPTTpqx9x0C5sDCnh4BGps/edit#gid=1719755213
+    return fetch(`https://docs.google.com/spreadsheets/d/${this.id}/gviz/tq?`)
+      .then((r) => r.ok ? r.text() : null)
+      .catch((_) => null)
+  }
+
+  filterUselessRows(rows) {
+    return rows.filter((row) => row && (row.v !== null && row.v !== undefined))
+  }
+
+  applyHeaderIntoRows(header, rows) {
+    return rows
+      .map(({ c: row }) => this.filterUselessRows(row))
+      .map((row) => row.reduce((p, c, i) => Object.assign(p, { [header[i]]: c.v }), {}))
+  }
+
+  getItems(spreadsheetResponse) {
+    let rows = []
+
+    try {
+      const parsedJSON = JSON.parse(spreadsheetResponse.split('\n')[1].replace(/google.visualization.Query.setResponse\(|\)\;/g, ''))
+      const hasLabelPropertyInCols = parsedJSON.table.cols.every(({ label }) => !!label)
+      if (hasLabelPropertyInCols) {
+        const header = parsedJSON.table.cols.map(({ label }) => label)
+
+        rows = this.applyHeaderIntoRows(header, parsedJSON.table.rows)
+      } else {
+        const [headerRow, ...originalRows] = parsedJSON.table.rows
+        const header = this.filterUselessRows(headerRow.c).map((row) => row.v)
+
+        rows = this.applyHeaderIntoRows(header, originalRows)
+      }
+    } catch (e) {}
+
+    return rows
+  }
+
+  async parse() {
+    const response = await this.getSheetsData()
+
+    if (response === null) {
+      return new Error(`Invalid sheets ID: ${this.id}`)
+    }
+
+    return this.getItems(response)
+  }
+}
