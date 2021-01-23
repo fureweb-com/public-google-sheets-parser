fcac68e3bf6f652ffabd1c8db8b7467d4a9f40f1
fureweb-com <fureweb.com@gmail.com>
2021-01-23 20:56:47 +0900

    Add dist file and remove package-lock.json

--- dist/index.js
+++ dist/index.js
@@ -9,13 +9,16 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 const isBrowser = typeof require === 'undefined';
-const fetch = isBrowser ? window.fetch : require('node-fetch');
+const fetch = isBrowser ?
+/* istanbul ignore next */
+window.fetch : require('node-fetch');
 
 let PublicGoogleSheetsParser = /*#__PURE__*/function () {
-  function PublicGoogleSheetsParser(spreadsheetId) {
+  function PublicGoogleSheetsParser(spreadsheetId, sheetName) {
     _classCallCheck(this, PublicGoogleSheetsParser);
 
     this.id = spreadsheetId;
+    this.sheetName = sheetName;
   }
 
   _createClass(PublicGoogleSheetsParser, [{
@@ -25,7 +28,17 @@ let PublicGoogleSheetsParser = /*#__PURE__*/function () {
       // It cannot be used unless everyone has been given read permission.
       // It must be a spreadsheet document with a header, as in the example document below.
       // spreadsheet document for example: https://docs.google.com/spreadsheets/d/10WDbAPAY7Xl5DT36VuMheTPTTpqx9x0C5sDCnh4BGps/edit#gid=1719755213
-      return fetch(`https://docs.google.com/spreadsheets/d/${this.id}/gviz/tq?`).then(r => r.ok ? r.text() : null).catch(_ => null);
+      // Sheet selection from: https://stackoverflow.com/a/44592363/1649917
+      if (!this.id) return null;
+      let url = `https://docs.google.com/spreadsheets/d/${this.id}/gviz/tq?`;
+
+      if (this.sheetName) {
+        url = url.concat(`sheet=${this.sheetName}`);
+      }
+
+      return fetch(url).then(r => r.ok ? r.text() : null).catch(
+      /* istanbul ignore next */
+      _ => null);
     }
   }, {
     key: "filterUselessRows",
@@ -69,15 +82,16 @@ let PublicGoogleSheetsParser = /*#__PURE__*/function () {
   }, {
     key: "parse",
     value: function () {
-      var _parse = _asyncToGenerator(function* (spreadsheetId) {
+      var _parse = _asyncToGenerator(function* (spreadsheetId, sheetName) {
         if (spreadsheetId) this.id = spreadsheetId;
+        if (sheetName) this.sheetName = sheetName;
         if (!this.id) throw new Error('SpreadsheetId is required.');
         const spreadsheetResponse = yield this.getSpreadsheetDataUsingFetch();
         if (spreadsheetResponse === null) return [];
         return this.getItems(spreadsheetResponse);
       });
 
-      function parse(_x) {
+      function parse(_x, _x2) {
         return _parse.apply(this, arguments);
       }
 
@@ -87,6 +101,8 @@ let PublicGoogleSheetsParser = /*#__PURE__*/function () {
 
   return PublicGoogleSheetsParser;
 }();
+/* istanbul ignore next */
+
 
 if (isBrowser) {
   window.PublicGoogleSheetsParser = PublicGoogleSheetsParser;
