84e69e50ab9b4eb5a4ac7972f009dcfc573e3ec9
fureweb-com <fureweb.com@gmail.com>
2021-03-16 22:10:02 +0900

    update nodeFecth implementation
    resolves #10

--- src/fetch.js
+++ src/fetch.js
@@ -3,10 +3,18 @@ const https = require('https')
 const nodeFetch = async (url) => {
   return new Promise((resolve, reject) => {
     const req = https.request(url, (res) => {
-      res.on('data', (data) => {
-        if (!String(data).startsWith('/*O_o*/')) return resolve(null)
+      const body = []
+      let isStarted = false
 
-        const response = { ok: true, text: () => String(data) }
+      res.on('data', (chunk) => {
+        if (!isStarted && !String(chunk).startsWith('/*O_o*/')) return resolve(null)
+        isStarted = true
+
+        body.push(chunk)
+      })
+
+      res.on('end', () => {
+        const response = { ok: true, text: () => Buffer.concat(body).toString() }
         resolve(response)
       })
     })
