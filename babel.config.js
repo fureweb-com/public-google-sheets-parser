494580278be7545e66a221db75131874a0466ea1
fureweb-com <fureweb.com@gmail.com>
2023-02-10 11:05:42 +0900

    Add exmaple page for gh-pages

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-const plugins = [
-  '@babel/plugin-transform-async-to-generator',
-  '@babel/plugin-transform-classes'
-]
-
-module.exports = {
-  plugins
-}
--- test/index.js
+++
@@ -1,7 +0,0 @@
-const TestBase = require('./base')
-
-const Source = new TestBase('../src/index.js')
-Source.test()
-
-const Dist = new TestBase('../dist/index.js')
-Dist.test()
