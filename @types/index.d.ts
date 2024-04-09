d22171b278f560e5d57bccf550c8c0ac3519e116
fureweb-com <fureweb.com@gmail.com>
2024-04-09 20:04:19 +0900

    update types for v1.5.0
    resolves #52

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,25 +1,32 @@
-type SheetName = string
-type GID = string
-
-type SheetInfo = SheetName | {
-  sheetName?: SheetName
-  sheetId?: GID
+interface ISheetOption {
+  sheetName?: TSheetName
+  sheetId?: TGID
+  useFormat?: boolean
+  useFormattedDate?: boolean
 }
 
+type TSpreadSheetId = string
+type TSheetName = string
+type TGID = string
+type TOption = TSheetName | ISheetOption
+
 declare class PublicGoogleSheetsParser {
-  private id?: string
-  private sheetName?: SheetName
-  private sheetId?: GID
+  private id?: TSpreadSheetId
+  private sheetName?: TSheetName
+  private sheetId?: TGID
+  private useFormat: boolean = false
+  private useFormattedDate: boolean = false
 
-  constructor(spreadsheetId?: string, sheetInfo?: SheetInfo)
+  constructor(spreadsheetId?: TSpreadSheetId, option?: TOption)
 
-  private parseSheetInfo(sheetInfo: SheetInfo): void
+  private isDate(date: string): boolean
   private getSpreadsheetDataUsingFetch(): Promise<string | null>
   private normalizeRow(rows: any[]): any[]
   private applyHeaderIntoRows(header: string[], rows: any[]): any[]
   private getItems(spreadsheetResponse: string): any[]
-
-  parse(spreadsheetId?: string, sheetInfo?: SheetInfo): Promise<any[]>
+  
+  setOption(option?: TOption): void
+  parse(spreadsheetId?: TSpreadSheetId, option?: TOption): Promise<any[]>
 }
 
 export default PublicGoogleSheetsParser
